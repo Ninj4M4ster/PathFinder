9e9bd50f7629e09dcf448e901eb8331438fada0f
Jakub Drzewiecki <kuba.drzewiecki@icloud.com>
2021-07-14 14:03:01 +0200

    Changed some styles, created window for pathfinder

--- pathfinder.py
+++ pathfinder.py
@@ -9,19 +9,19 @@ class MainWindow(QtWidgets.QWidget):
 
         self.titleLabel = QtWidgets.QLabel(self)
         self.titleLabel.setText("Welcome to path finding visualisation")
+        self.titleLabel.setObjectName("titleLabel")
 
         self.descriptionLabelOne = QtWidgets.QLabel(self)
         self.descriptionLabelOne.setText("Pick an algorithm from all available.")
-        self.descriptionLabelOne.move(0, 15)
+        self.descriptionLabelOne.move(0, 50)
 
         self.descriptionLabelTwo = QtWidgets.QLabel(self)
         self.descriptionLabelTwo.setText("Set start and end points, create obstacles if you need them and run the code!")
-        self.descriptionLabelTwo.move(0, 30)
+        self.descriptionLabelTwo.move(0, 65)
 
 
         self.algorithms = QtWidgets.QLabel(self)
-        self.algorithms.move(0, 45)
-        self.algorithms.setGeometry(0, 45, 1000, 50)
+        self.algorithms.setGeometry(0, 80, 1000, 50)
 
         self.algorithmButtonsLayout = QtWidgets.QGridLayout(self.algorithms)
 
@@ -52,6 +52,53 @@ class MainWindow(QtWidgets.QWidget):
         self.seventhAlgorithm = QtWidgets.QPushButton(self.algorithms)
         self.seventhAlgorithm.setText("Random Walk")
         self.algorithmButtonsLayout.addWidget(self.seventhAlgorithm, 0, 6)
+
+        self.centralWidget = QtWidgets.QLabel(self)
+        self.centralWidget.setGeometry(0, 130, 1055, 530)
+        self.centralWidget.setObjectName("central")
+
+        for i in range(50):
+            for j in range(25):
+                square = QtWidgets.QPushButton(self.centralWidget)
+                square.setMinimumSize(20, 20)
+                square.setMaximumSize(20, 20)
+                square.setObjectName(f"square {i} {j}")
+                square.move(i*21 + 3, j*21 + 3)
+                square.clicked.connect(self.on_clicked)
+
+
+
+
+        stylesheetstr = """
+            QLabel{
+                background-color: red;
+            }
+            #titleLabel{
+                position:relative;
+                right:100px;
+                font-size:40px;
+            }
+            #central{
+                background-color:grey;
+                border:2px solid black;
+            }
+            QLabel#central QPushButton{
+                background-color:black;
+                width:10px;
+                height:20px;
+                transition: 0.5s;
+            }
+            QLabel#central QPushButton:hover{
+                background-color:white;
+                
+            }
+        """
+        self.setStyleSheet(stylesheetstr)
+
+    def on_clicked(self):
+        sender = self.sender()
+        senderName = sender.objectName()
+        print(senderName)
 
 
 def main():
